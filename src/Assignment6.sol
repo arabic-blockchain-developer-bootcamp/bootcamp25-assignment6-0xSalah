79077a485cc0a6bf5226cb534e1f534aee29b93d
salah <vvizks@gmail.com>
2025-03-07 04:31:13 +0300

    solved assignments 6

--- src/Assignment6.sol
+++ src/Assignment6.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.13;
 
 contract Assignment6 {
     // 1. Declare an event called `FundsDeposited` with parameters: `sender` and `amount`
+    event FundsDeposited (address sender, uint amount);
 
     // 2. Declare an event called `FundsWithdrawn` with parameters: `receiver` and `amount`
+    event FundsWithdrawn (address receiver, uint amount);
 
     // 3. Create a public mapping called `balances` to tracker users balances
+    mapping (address => uint) public balances;
 
     // Modifier to check if sender has enough balance
     modifier hasEnoughBalance(uint amount) {
+        require(balances[msg.sender] >= amount);
         // Fill in the logic using require
         _;
     }
@@ -18,10 +22,13 @@ contract Assignment6 {
     // This function should:
     // - Be external and payable
     // - Emit the `FundsDeposited` event
-    function deposit() {
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
         // increment user balance in balances mapping 
 
         // emit suitable event
+        emit FundsDeposited (msg.sender,msg.value);
+
     }
 
     // Function to withdraw Ether
@@ -30,12 +37,15 @@ contract Assignment6 {
     // - Take one parameter: `amount`
     // - Use the `hasEnoughBalance` modifier
     // - Emit the `FundsWithdrawn` event
-    function withdraw() {
+    function withdraw(uint amount) external hasEnoughBalance(amount) {
         // decrement user balance from balances mapping 
+        balances[msg.sender] -= amount;
 
         // send tokens to the caller
+        payable(msg.sender).transfer(amount);
 
         // emit suitable event
+        emit FundsWithdrawn (msg.sender,amount);
 
     }
 
@@ -43,8 +53,8 @@ contract Assignment6 {
     // This function should:
     // - Be public and view
     // - Return the contract's balance
-    function getContractBalance() {
-        // return the balance of the contract
+    function getContractBalance() public view returns(uint){
+       return address(this).balance;
 
     }
 }
